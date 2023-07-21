63a2cfc395f38e6a03a70a3534efb6f32ab92a79
Mariusz Trocewicz <throcu@gmail.com>
2023-07-21 02:00:54 +0200

    fix: changed contacts to async/await

--- contacts.js
+++ contacts.js
@@ -1,67 +1,58 @@
-const fs = require('fs');
+const fs = require('fs').promises;
+const { error, log } = require('console');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 const contactsPath = path.normalize('./db/contacts.json');
 
-function listContacts() {
-  fs.readFile(contactsPath, function (error, data) {
-    if (error) {
-      console.log('error', error.message);
-      return;
-    }
-    console.log(JSON.parse(data));
-  });
-}
+const listContacts = async () => {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    console.log(JSON.parse(contacts));
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-function getContactById(contactId) {
-  fs.readFile(contactsPath, function (error, data) {
-    if (error) {
-      console.log('error', error.message);
-      return;
-    }
-    const parsedData = JSON.parse(data);
-    const findId = parsedData.filter(contact => contact.id.includes(contactId));
+const getContactById = async contactId => {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    const parsedData = JSON.parse(contacts);
+    const filteredContacts = parsedData.filter(contact => contact.id.includes(contactId));
+    console.log(filteredContacts);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-    console.log(JSON.stringify(findId));
-  });
-}
-
-function removeContact(contactId) {
-  fs.readFile(contactsPath, function (error, data) {
-    if (error) {
-      console.log('error', error.message);
-      return;
-    }
-    const parsedData = JSON.parse(data);
+const removeContact = async contactId => {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    const parsedData = JSON.parse(contacts);
     const newContacts = parsedData.filter(contact => contact.id !== contactId);
-    fs.writeFile(contactsPath, JSON.stringify(newContacts, null, 2), error => {
-      if (error) {
-        console.log('Failed to write updated data to file');
-        return;
-      }
-      console.log('Updated file successfully');
-    });
-  });
-}
+    await fs.writeFile(contactsPath, JSON.stringify(newContacts, null, 2));
+    console.log('Updated file successfully');
+  } catch (error) {
+    console.error('Failed to write updated data to file:', error);
+  }
+};
 
-function addContact(name, email, phone) {
-  fs.readFile(contactsPath, function (error, data) {
-    if (error) {
-      console.log('error', error.message);
-      return;
-    }
-    const parsedData = JSON.parse(data);
-    const newContacts = parsedData.concat({ id: uuidv4(), name: name, email: email, phone: phone });
-    fs.writeFile(contactsPath, JSON.stringify(newContacts, null, 2), error => {
-      if (error) {
-        console.log('Failed to write updated data to file');
-        return;
-      }
-      console.log('Updated file successfully');
+const addContact = async (name, email, phone) => {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    const parsedData = JSON.parse(contacts);
+    const newContacts = parsedData.concat({
+      id: uuidv4(),
+      name: name,
+      email: email,
+      phone: phone,
     });
-  });
-}
+    await fs.writeFile(contactsPath, JSON.stringify(newContacts, null, 2));
+    console.log('Updated file successfully');
+  } catch (error) {
+    console.error('Failed to write updated data to file:', error);
+  }
+};
 
 module.exports = {
   listContacts,
